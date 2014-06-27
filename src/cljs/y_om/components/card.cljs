9636ec50b5b357cb7bd5770926dd0b0ff3e4ed81
Will Sommers <will.sommers@gmail.com>
2014-06-26 22:44:10 -0400

    Move to using defcomponent for card.cljs, remove debugging statement.

--- src/cljs/y_om/components/card.cljs
+++ src/cljs/y_om/components/card.cljs
@@ -3,29 +3,23 @@
   (:require
    [cljs.core.async :as async :refer [>! <! alts! chan sliding-buffer put! close!]]
    [y-om.utils :as utils]
-   [om.dom :as dom :include-macros true]
    [om.core :as om :include-macros true]
-   [sablono.core :as html :refer-macros [html]]
-   [y-om.app-state :as app-state]))
+   [om-tools.core :refer-macros [defcomponent]]
+   [om-tools.dom :as dom :include-macros true]))
 
+(defcomponent card-component [data owner]
 
+  (init-state [_]
+    {:c-drag-card (chan)
+     :dragging false})
 
-(defn card-component [data owner]
-  (reify
-    om/IInitState
-    (init-state [_]
-      {:c-drag-card (chan)
-       :dragging false})
+  (will-mount [_]
+    (let [c-drag-card (om/get-state owner :c-drag-card)]
+      (go (while true
+            (let [pos (<! c-drag-card)]
+              )))))
 
-    om/IWillMount
-    (will-mount [_]
-      (let [c-drag-card (om/get-state owner :c-drag-card)]
-        (go (while true
-              (let [pos (<! c-drag-card)]
-                (println pos))))))
-    om/IRenderState
-    (render-state [_ {:keys [dragging c-drag-card]}]
-      (let [c-column-control (om/get-state owner :c-column-control)]
-        (html [:div.list-card {:on-click #(if-not dragging (put! c-column-control (:id @data)))
-                               }
-               [:div (:task data)]])))))
+  (render-state [_ {:keys [dragging c-drag-card]}]
+    (let [c-column-control (om/get-state owner :c-column-control)]
+      (dom/div {:class "list-card"}
+        (dom/div (:task data))))))
