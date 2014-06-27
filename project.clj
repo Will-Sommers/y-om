42e5485d1c924075de6a84c65d7f7ed6bd749390
Will Sommers <will.sommers@gmail.com>
2014-06-26 21:24:44 -0400

    Bumping Clojure, ClojureScript and Om in project.clj

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2234"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [om "0.6.2"]
                  [ankha "0.1.1"]
                  [sablono "0.2.17"]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
 
   :source-paths ["src"]
 
