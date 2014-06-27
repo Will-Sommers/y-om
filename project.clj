2d9a3def1770ebd60e1aaaf492015d0575a9731a
Will Sommers <will.sommers@gmail.com>
2014-06-26 21:48:58 -0400

    Bump Om, include om-tools in project.clj.

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2234"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [om "0.6.2"]
+                 [om "0.6.4"]
+                 [prismatic/om-tools "0.2.2"]
                  [ankha "0.1.1"]
                  [sablono "0.2.17"]]
 
