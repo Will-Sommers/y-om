6e685113ea823a504e2a84faf676819123156567
Will Sommers <will.sommers@gmail.com>
2014-06-28 10:56:34 -0400

    Example 2 for tutorial.

---
+++ example-code/ex2.cljs
@@ -0,0 +1,25 @@
+(def app-state (atom {:board-name "Yo yo yo, get it done!"
+                      :columns [{:name "To Do"}
+                                {:name "Doing"}
+                                {:name "Done"}]}))
+
+(defn column-component [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:className "column"}
+               (dom/div nil (:name data))
+               (dom/div #js {:className "cards-wrapper"}
+                        (dom/div nil "Cards go here"))))))
+
+(defn app-component [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (apply dom/div nil
+        (om/build-all column-component (:columns data))))))
+
+(om/root
+  app-component
+  app-states
+  {:target (. js/document (getElementById "app"))})
